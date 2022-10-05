3be47c7bb93145f6d0471f1a2440f2d02867701f
hyeon <einsight1@naver.com>
2022-10-05 10:29:53 +0900

    divide function edited

--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -9,14 +9,15 @@ class Calculator{
         System.out.println("a * b is "+(a*b));
     }
     public void divide(int a, int b){
-        System.out.println("a / b is "+(double)(a/b));
+        double result = (double)a / b;
+        System.out.println("a / b is "+result);
     }
 }
 public class CalculatorMain {
     public static void main(String[] args) {
         Calculator c = new Calculator ();
 
-        c.divide(2,1);
+        c.divide(1,2);
         c.plus(2,1);
         c.minus(2,1);
         c.multiply(2,1);
