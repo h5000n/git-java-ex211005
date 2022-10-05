ec17a2f260879c107548f8d25171b78acfedfc76
hyeon <einsight1@naver.com>
2022-10-05 09:33:54 +0900

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello{
+    private int a = 10;
+    private int b = 20;
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp = a;
+
+        a= b;
+        b = temp;
+        System.out.printf("a: %d b:%d ",a,b);
+    }
+}
